3dc81b75f84a647dc4fdc6bb96631a16aed2ce60
Eric George <ericgeo324@gmail.com>
2023-11-03 14:10:12 -0400

    astro config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site:'https://myr124.github.io',
-  base: '/blogbase',
+  base: '/',
   integrations: [react(), tailwind({
     applyBaseStyles:false
   })]
